aa33e10bac86e0f1955bc2530adc4c602432d59b
Juanbeleño <juanb7737@gmail.com>
2024-03-21 10:47:08 -0300

    finalizacion de consignas

--- assets/js/deposit.js
+++ assets/js/deposit.js
@@ -1,14 +1,20 @@
-const formulario = document.getElementById(formulario)
-const nombre = document.getElementById(nombre)
-const number = document.getElementById(cantidad)
-function deposit(e){
-    e.preventDefault()
-    if(nombre !== "" && number !== null){
-        let confirmacion = confirm($`¿Estás seguro de depositar ${number} a ${nombre}?`)
-        if(confirmacion){
-            alert("su deposito ha sido relaizado con exito")
+const formulario = document.getElementById('formulario');
+
+function deposit(e) {
+    e.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const cantidad = document.getElementById('cantidad').value;
+
+    if(nombre !== "" && cantidad !== "") {
+        let confirmacion = confirm(`¿Estás seguro de depositar ${cantidad} a ${nombre}?`);
+
+        if(confirmacion) {
+            alert("Su depósito ha sido realizado con éxito");
+        }else{
+            alert("Operación cancelada");
         }
-    
     }
 }
-formulario.addEventListener("submit", deposit)
+
+formulario.addEventListener("submit", deposit);
